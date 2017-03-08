8f0c19a68974d7d0b3b5514303b9464c0d946659
Caroline Fukuchi <cfukuchi@umich.edu>
2017-03-07 19:22:36 -0500

    fixing strato likelihood function

--- stratoML.py
+++ stratoML.py
@@ -6,6 +6,8 @@ from scipy import optimize
 def hr97_like(tree,strat,lam):
     lik = []
     for i in tree.leaves():
+        if i.occurrences == []:
+            continue
         if i.occurrences[0] == "NA" or i.occurrences == None: #TODO figure out what to do with ghost lineages
             continue
         #elif len(i.occurrences) == 1 and i.occurrences[0]: 
@@ -16,14 +18,14 @@ def hr97_like(tree,strat,lam):
             f = max(i.occurrences)
             l = min(i.occurrences)
             num = len(i.occurrences)
-            a = math.pow((l-f),(num-2))
+            a = math.pow((abs(l-f)),(num-2))
             b = math.pow(lam,num)
             c = math.exp(-lam*(tl-tf))
             top = a*b*c
             bot = math.factorial(num-2)
-            print str(top)+" "+str(bot)
+            #print str(top)+" "+str(bot)
             loglik = math.log(top/bot)
         elif len(i.occurrences) == 1:
-            loglik = math.log(1/(tl-tf))
+            loglik = math.log(1/(abs(tl-tf)))
         lik.append(loglik)
-    return sum(lik)
+    return -sum(lik)
